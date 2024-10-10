4129849cf632fe14d287a010a0110db10e5944e3
AnnaJuhlia <julia.silva.anna@escola.pr.gov.br>
2024-10-10 12:21:50 -0300

    ccc

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ cartao.innerHTML = `
 <div class=conteudo-cartao>
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
-        <p>pergunta</p>
+        <p>${pergunta}</p>
     </div>
     <div class="resposta-cartao">
         <p>resposta</p>
